ee48572c722294ab04b04dfd41ebae789f0c67a8
Frederic Chhean <chheanfrederic@gmail.com>
2019-05-17 14:40:00 +0200

    ,,

--- src/components/profile/ProfileHeader.js
+++ src/components/profile/ProfileHeader.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import "./ProfileHeader.css"
+import "./ProfileHeader.css";
+import isEmpty from "../../validation/is-empty";
+import Logolinkedin from "../profile/linkedin (3).png"
+import Logodribble from "../profile/dribble-logo.png"
+import Logogithub from "../profile/github-sign.png"
+import Logostar from "../profile/star (3).png"
+
+
 
 class ProfileHeader extends Component {
   render() {
@@ -19,16 +26,36 @@ function newFunction(profile) {
 
 
     <img className="card-img-top img-ind" src={profile.user.avatar} alt="Card image cap" style={{ width: "15rem" }} />
+    <div className="nameforindivcard">{profile.user.firstname} {profile.user.lastname}</div>
+    <div className="nameforindivcard2">{profile.title}</div>
+    <div className="nameforindivcard3">"{profile.tagline}"</div>
+
+
     <div>
+    
+    <div className="card card1" style={{ width: "60rem" }}>
+      
+    <div className="card-body card-body1" style={{ width: "60rem" }}>
+          <h4 className="card-title titlesocial">Social Networks</h4>
+          <div>
+          <a className="logonetwork" href={profile.linkedin}><img src={Logolinkedin}/></a>
+          <a className="logonetwork" href={profile.linkedin}><img src={Logodribble}/></a>
+          <a className="logonetwork" href={profile.linkedin}><img src={Logogithub}/></a>
+          <a className="logonetwork" href={profile.linkedin}><img src={Logostar}/></a>
+          </div>
+        </div>
+        </div>
+
       
       <div className="card card1" style={{ width: "60rem" }}>
+ 
         <div className="card-body card-body1" style={{ width: "34rem" }}>
-          <h4 className="card-title">Hero Experience</h4>
-          <p className="card-text">{profile.user.title}</p>
+          <h4 className="card-title titlesocial">Hero Experience</h4>
+          <p className="card-text">{profile.heroexperience}</p>
         </div>
 
         <div className="card-body card-body1" style={{ width: "34rem" }}>
-          <h4 className="card-title">Skills</h4>
+          <h4 className="card-title titlesocial">Skills</h4>
           {/* <p className="card-text">{profile.user.skills}</p> */}
           <p className="card-text">
             {profile.skills.slice(0,10). map((skill, index) => (
@@ -42,7 +69,7 @@ function newFunction(profile) {
         <hr></hr>
         <div>
           <div className="card-body" style={{ width: "20rem" }}>
-            <h4 className="card-title">Languages</h4>
+            <h4 className="card-title titlesocial">Languages</h4>
             {/* <p className="card-text">{profile.user.languages}</p> */}
             <p className="card-text">
             {profile.languages.slice(0,10). map((language, index) => (
